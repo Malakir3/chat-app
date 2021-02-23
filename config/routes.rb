29dc92924531fada4668491c20b132df33bcd7a6
Malakir3 <k.yasuyuki0924@gmail.com>
2021-02-23 14:52:08 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # get 'messages/index' ⇦4841の回答で消えてた
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
